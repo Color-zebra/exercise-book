24aa0a1076d4fb269fadaf85c59d4ad5ac4df56a
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-12 17:30:12 +0300

    function for random noninteger number from min to max

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
 "use strict"
 
+function randomNumber(min, max) {
+	return Math.random()*(max-min) + min;
+}
+
+console.log(randomNumber(1, 10));
+console.log(randomNumber(-100, 1));
+console.log(randomNumber(0, 1));

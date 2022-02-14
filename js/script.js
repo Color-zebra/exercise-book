17b5755de5dcdf52e7033166657cc5d10d792b5d
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-14 16:54:42 +0300

    metods of arrays fourth part

--- js/script.js
+++ js/script.js
@@ -1,28 +1,58 @@
 "use strict"
 
 //===========================================================================================================
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
-let users = [ vasya, petya, masha ];
+let array = [ vasya, petya, masha ];
 
-let usersMapped = mapping(users);
+function sortByAge(arr) {
+	let arrCopy = arr.slice(0);
+	return arrCopy.sort((a, b) => a.age - b.age);
+};
 
-function mapping(arr) {
-	return arr.map(user => ({
-		fullName: user.name + ' ' + user.surname,
-		id: user.id,
-	}))
-} 
+let sortedArr = sortByAge(array);
 
-/*
-usersMapped = [
-	{ fullName: "Вася Пупкин", id: 1 },
-	{ fullName: "Петя Иванов", id: 2 },
-	{ fullName: "Маша Петрова", id: 3 }
-]
-*/
-console.log( usersMapped )
-console.log( usersMapped[0].id ) // 1
-console.log( usersMapped[0].fullName ) // Вася Пупкин
+console.log(array);
+console.log(sortedArr)
+
+// теперь: [vasya, masha, petya]
+console.log(sortedArr[0].name); // Вася
+console.log(sortedArr[1].name); // Маша
+console.log(sortedArr[2].name); // Петя
+
+
+//===========================================================================================================
+function averageAge(arr) {
+	return arr.reduce((sum, item) => (sum + item.age), 0)/arr.length;
+}
+
+console.log(averageAge(array));
+
+
+//===========================================================================================================
+function unique(arr) {
+	let result = [];
+	arr.forEach(item => result.includes(item) ? '' : result.push(item))
+	return result;
+}
+
+let strings = ["кришна", "кришна", "харе", "харе",
+	"харе", "харе", "кришна", "кришна", ":-O"
+];
+
+console.log( unique(strings) ); // кришна, харе, :-O
+
+
+//===========================================================================================================
+let arr = [1, 2, 3];
+
+function shuffle(arr) {
+	let result = arr.slice(0);
+	return result.sort((a, b) => (Math.random()-0.5))
+}
+
+console.log(shuffle(arr));
+console.log(shuffle(arr));
+console.log(shuffle(arr));

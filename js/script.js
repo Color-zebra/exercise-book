5c978b391c11a5121707e01263fa98fac0388671
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-14 15:05:31 +0300

    metods of arrays first part

--- js/script.js
+++ js/script.js
@@ -1,59 +1,96 @@
 "use strict"
 
 
-function camelize(str) {
-	if (str) return str
-	.split('-')
-	.map((item, index) => index === 0 ? item : item[0].toUpperCase() + item.slice(1))
-	.join('');
-	return str;
-}
-
-console.log(camelize('на-золотом-крыльце-сидели'));
-console.log(camelize(''));
-
-
-//------------------------------------------------------------------------------------------------------------------
-function filterRange(arr, a, b) {
-	return arr.filter((item) => item >= a && item <= b);
-};
-
-let arrrr = [5, 3, 8, 1];
-let filtered = filterRange(arrrr, 1, 4);
-console.log(arrrr);
-console.log(filtered);
-
-
-//------------------------------------------------------------------------------------------------------------------
-function filterRangeInPlace(arr, a, b) {
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] < a || arr[i] > b) {
-			arr.splice(i, 1);
+/* function Calculator() {
+	this.calculate = function(str) {
+		let arr = str.split(' ');
+		console.log(arr);
+		let firstNumber = arr[0];
+		let secondNumber = arr[2];
+		let operator = arr[1];
+		console.log(firstNumber, secondNumber, operator);
+		switch(operator) {
+			case '+':
+				return +firstNumber + +secondNumber;
+			case '-':
+				return +firstNumber - +secondNumber;
 		};
+	};
+	this.addMethod = function(name, func) {
+
+	}
+} */
+
+//ХЕРНЯ! Работает, но хер знает как расширить.
+
+function Calculator() {
+	this.methods = {
+		'+':((a, b) => a + b),
+		'-':((a, b) => a - b),
+	}
+	this.calculate = function(str) {
+		let first = +str.split(' ')[0];
+		let second = +str.split(' ')[2];
+		let operator = str.split(' ')[1];
+		if ( !(this.methods[operator] && isFinite(first) && isFinite(second))) {
+			return 'Uncorrect insert'
+		}
+		return this.methods[operator](first, second);
+	};
+	this.addMethod = function(name, func) {
+		this.methods[name] = func
 	};
 }
 
-let array = [5, 3, 8, 1, 2, 3, 6, 7, 8];
-console.log(array);
-filterRangeInPlace(array, 3, 8);
-console.log(array);
+let test = new Calculator();
+console.log(test);
+
+console.log(test.calculate( '2 + 3')); //5
+console.log(test.calculate( '10 - 8')); //2
+console.log(test.calculate( '7 - 20')); //-13
 
 
-//------------------------------------------------------------------------------------------------------------------
-function sortFromMaxToMin(arr) {
-	return arr.sort((a,b) => b-a)
+test.addMethod('*', (a, b) => a * b);
+test.addMethod('/', (a, b) => a / b);
+test.addMethod(`**`, (a, b) => a ** b);
+console.log(test);
+
+console.log(test.calculate( '10 / 5')); //2
+console.log(test.calculate( '10 * 8')); //80
+console.log(test.calculate( '2 ** 3')); //8
+
+
+console.log(test.calculate( 'помидор / апельсин')); //2
+console.log(test.calculate( 'синее * 8')); //80
+console.log(test.calculate( 'Infinity ** 3')); //8
+console.log(test.calculate( '3 % 3')); //8
+
+
+//===========================================================================================================
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+function namesArr(arr) {
+	let result = [];
+	for (let item of arr) {
+		result.push(item.name)
+	}
+	return result;
 }
 
-console.log(sortFromMaxToMin(array))
-
-
-//------------------------------------------------------------------------------------------------------------------
-function copySorted(arr) {
-	return arr.slice().sort()
+function namesArrAnother(arr) {
+	return arr.map(item => item.name);
 }
 
-let arr = ["HTML", "JavaScript", "CSS"];
-let sorted = copySorted(arr);
+let names = namesArr(users);
+let namesAnother = namesArrAnother(users);
 
-console.log( sorted ); // CSS, HTML, JavaScript
-console.log( arr ); // HTML, JavaScript, CSS (без изменений)
+console.log( names ); // Вася, Петя, Маша
+console.log( users );
+console.log( namesAnother ); // Вася, Петя, Маша
+
+
+//===========================================================================================================

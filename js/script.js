9c77607b45d4a235a46a7dcf3ae74de68364b699
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-16 20:42:59 +0300

    Destructuring assignment

--- js/script.js
+++ js/script.js
@@ -1,112 +1,56 @@
 "use strict"
 
 //===========================================================================================================
-// 		Объект можно сделать итерируемым, если добавить ему ключ [Symbol.iteration], с значением в виде функции,
-// которая в свою очередь возвращает объект, содержащий в себе свойство current: начальное значение, свойство last: конечное значениеб
-// и функцию next(), которая описывает как именно "прогонять" каждый цикл итерации, вовращает объект, в котором обязательно должно быть
-// свойство done: со значением true или false, которое показывает, нужно ли итерации делать еще один "прогон". В случае если итерация продолжается,
-// необходимо выполнить какие-либо действия со счетчиком итерации current, что бы не сделать бесконечный цикл.
+// 		Деструктурирующее присваивание позволяет нам коротким способом записать элементы массива/свойства объекта в отдельные переменные.
+// 		1) Можно задавать значения по умолчанию.
+// 		2) При деструктуризации объектов можно перезаписывать текущие свойства в переменные с другим названием (по умолчанию переменная должна соответствовать названию свойства)
+// 		3) Умные свойства функции. В некоторые функции необходимо передавать множество свойств. В это случае проблемой становится запомнить их последовательность, для передачи в верно порядке.
+// Мы можем передать свойства в виде объекта, с ключами, соответствующими названиям параметров функции. Функция автоматически деструктуризирует объект на переменные.
+// Учитывая возможность задавать значения по умолчанию, использование деструктуризирующего присваивания таким образом сильно облегчает использование функций.
 
-let range = {
-	from: 1,
-	to: 5,
+let user = {
+	name: "John",
+	years: 30
 };
 
-range[Symbol.iterator] = function() {
-	return {
-		current: this.from,
-		last: this.to,
-		next() {
-			if (this.current <= this.last) {
-				return {
-					done: false,
-					value: this.current++,
-					};
-				} else {
-					return {
-						done: true,
-					};
-				}
-			},
-		};
-	};
+let {name, years: age, isAdmin = false} = user;
 
+console.log( name ); // John
+console.log( age ); // 30
+console.log( isAdmin ); // false
 
-for (let item of range) {
-	console.log(item);
-};
-
-let daysOfTheWeek = {
-	0: 'monday',
-	1: 'tuesday',
-	2: 'wednesday',
-	3: 'thursday',
-	4: 'friday',
-	5: 'saturday',
-	6: 'sunday',
-	length: 7,
-	
-};
-daysOfTheWeek[Symbol.iterator] = function() {
-	return {
-		current: 0,
-		last: this.length,
-		next() {
-			if (this.current !== this.last) {
-				return {
-					done: false,
-					value: daysOfTheWeek[this.current++],
-				}
-			} else if (this.current == this.last){
-				return {
-					done: true,
-				}
-			};
-		},
-	};
-};
-
-for (let item of daysOfTheWeek) {
-	console.log(item)
-};
-
-let arr = Array.from(daysOfTheWeek);
-console.log(arr);
 
 //===========================================================================================================
-// 		Так же для объекта есть и встроенные методы итерации Object.keys(obj), Object.values(obj), Object.entries(obj)
-// в отличии от похожих методов в Set и Map, данные методы возвращают реальный массив значений, а не просто итерируют объект.
-// Обратить ВНИМАНИЕ, что символьные свойства игнорируюся. Для них слудет использовать либо Object.getOwnPropertySymbols(), либо Reflect.ownKeys(obj).
-
 let salaries = {
 	"John": 100,
 	"Pete": 300,
 	"Mary": 250
 };
+let empty = {};
 
-function sumSalaries(obj) {
-	let result = 0;
-	for (let item of Object.values(obj)) {
-		result += item;
+function topSalary(salaries) {
+	let namesArr = []
+	for (let item of Object.entries(salaries)) {
+		namesArr.push(item[0]);
 	}
-	return result;
-}
-
-function sumSalaries2(obj) {
-	return Object.values(obj).reduce((previous, current) => previous + current, 0)
-}
-
-console.log( sumSalaries(salaries) ); // 650
-console.log( sumSalaries2(salaries) ); // 650
-
-
-//===========================================================================================================
-let user = {
-	name: 'John',
-	age: 30
+	if (!namesArr[0]) return null;
+	return namesArr.sort((a, b) => salaries[b] - salaries[a])[0];
 };
 
-function count(obj) {
-	return Object.keys(obj).length
+console.log(topSalary(salaries));
+console.log(topSalary(empty));
+
+function topSalary2(obj) {
+	let max = 0;
+	let maxName = null;
+	for (let [name, value] of Object.entries(obj)) {
+		if (value >= max) {
+			max = value;
+			maxName = name;
+		};
+	};
+	return maxName;
 }
-console.log ( count(user) ); // 2
+
+console.log(topSalary2(salaries));
+console.log(topSalary2(empty));

e5d9b1e63a41e2412fffad0374cbd398883bdd81
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-16 19:21:50 +0300

    added more ways to iterate objects

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 "use strict"
 
 //===========================================================================================================
+// 		Объект можно сделать итерируемым, если добавить ему ключ [Symbol.iteration], с значением в виде функции,
+// которая в свою очередь возвращает объект, содержащий в себе свойство current: начальное значение, свойство last: конечное значениеб
+// и функцию next(), которая описывает как именно "прогонять" каждый цикл итерации, вовращает объект, в котором обязательно должно быть
+// свойство done: со значением true или false, которое показывает, нужно ли итерации делать еще один "прогон". В случае если итерация продолжается,
+// необходимо выполнить какие-либо действия со счетчиком итерации current, что бы не сделать бесконечный цикл.
 
 let range = {
 	from: 1,
@@ -67,3 +72,41 @@ for (let item of daysOfTheWeek) {
 
 let arr = Array.from(daysOfTheWeek);
 console.log(arr);
+
+//===========================================================================================================
+// 		Так же для объекта есть и встроенные методы итерации Object.keys(obj), Object.values(obj), Object.entries(obj)
+// в отличии от похожих методов в Set и Map, данные методы возвращают реальный массив значений, а не просто итерируют объект.
+// Обратить ВНИМАНИЕ, что символьные свойства игнорируюся. Для них слудет использовать либо Object.getOwnPropertySymbols(), либо Reflect.ownKeys(obj).
+
+let salaries = {
+	"John": 100,
+	"Pete": 300,
+	"Mary": 250
+};
+
+function sumSalaries(obj) {
+	let result = 0;
+	for (let item of Object.values(obj)) {
+		result += item;
+	}
+	return result;
+}
+
+function sumSalaries2(obj) {
+	return Object.values(obj).reduce((previous, current) => previous + current, 0)
+}
+
+console.log( sumSalaries(salaries) ); // 650
+console.log( sumSalaries2(salaries) ); // 650
+
+
+//===========================================================================================================
+let user = {
+	name: 'John',
+	age: 30
+};
+
+function count(obj) {
+	return Object.keys(obj).length
+}
+console.log ( count(user) ); // 2

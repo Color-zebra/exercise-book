6b54ea00521f01e89dc5dbd54469478e70fceb63
Romanenkov Dmitriy <215rd@mail.ru>
2022-02-16 15:16:14 +0300

    Map and Set

--- js/script.js
+++ js/script.js
@@ -1,52 +1,70 @@
 "use strict"
 
-//==================================WITHOUT CLOSURE=========================================================================
+//==================================MAP=========================================================================
 
-let count = 0;
-function increaseCounter() {
-	count++;
-	console.log(count);
+// Как и обычный объект это коллекция пар КЛЮЧ - ЗНАЧЕНИЕ. 
+// Основное отличие в том, что КЛЮЧОМ МОЖЕТ БЫТЬ НЕ ТОЛЬКО СТРОКА.
+// К примеру число, булево значения, или даже другой объект.
+
+//==================================SET=========================================================================
+
+// Это особая коллекция, без ключей, только значения.
+// Особенность в том, что set ХРАНИТ ТОЛЬКО УНИКАЛЬНЫЕ ЗНАЧЕНИЯ.
+// При попытке записать в него уже существующее значени, оно не запишется.
+
+
+//==============================================================================================================
+function unique(arr) {
+	return Array.from(new Set(arr));
 }
 
-/* increaseCounter();
-increaseCounter();
-increaseCounter();
-increaseCounter();
-increaseCounter();
-increaseCounter();
-console.log(count) */
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+	"Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(values) ); // Hare,Krishna,:-O
+
+let uni = unique(values);
+console.log( uni );
 
 
-//==================================WITH CLOSURE=========================================================================
-function createCounter(startingValue) {
-	let count = startingValue;
-	return () => {
-		count++;
-		console.log(count)
+//==============================================================================================================
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+function aclean(arr) {
+	let set = new Set();
+	let result = [];
+	for (let item of arr) {
+		let prop = item.toUpperCase().split('').sort().join('');
+		if ( !set.has(prop) ) {
+			set.add(prop);
+			result.push(item);
+		}
 	}
+	return result;
+}
+
+function aclean2(arr) {
+	let map = new Map();
+	for (let item of arr) {
+		map.set((item.toLowerCase().split('').sort().join('')), item);
+	}
+	return Array.from(map.values());
 };
 
-let firstCounter = createCounter(5);
-let secondCounter = createCounter(10);
-let thirdCounter = createCounter(15);
+console.log( 'Через MAP', aclean2(arr) );
+console.log( 'Через SET', aclean(arr) ); // "nap,teachers,ear" или "PAN,cheaters,era"
 
-firstCounter();
-firstCounter();
-firstCounter();
 
-console.log('//=======================================')
+//==============================================================================================================
+let map = new Map();
 
-secondCounter();
+map.set("name", "John");
 
-console.log('//=======================================')
+let keys = Array.from(map.keys());
+console.log(keys);
 
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
-thirdCounter();
+keys.push("more");
+console.log(keys);
+
